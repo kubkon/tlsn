82ee78f565bc2d750b5535443b8325b7576b4e28
kubkon <kubkon@users.noreply.github.com>
2025-05-28 05:12:59 +0000

    deploy: b6845dfc5c1d0b3d3178b3aff6f7da83e98f00d7

---
+++ tlsn_wasm/types/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Body","KeyType","Method","NetworkSetting","TlsVersion"],"struct":["Attestation","Commit","ConnectionInfo","HttpRequest","HttpResponse","NotarizationOutput","PartialTranscript","Presentation","PresentationOutput","Reveal","Secrets","Transcript","TranscriptLength","VerifierOutput","VerifyingKey"]};
